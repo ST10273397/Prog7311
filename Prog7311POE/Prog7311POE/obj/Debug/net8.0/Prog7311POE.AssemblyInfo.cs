d69cedc5d963b61d18952fa92a0f942dfe137410
Nicholas Liam Wolfaardt <ST10273397@vcconnect.edu.za>
2025-06-24 23:20:58 +0200

    UI Update

--- Prog7311POE/Prog7311POE/obj/Debug/net8.0/Prog7311POE.AssemblyInfo.cs
+++ Prog7311POE/Prog7311POE/obj/Debug/net8.0/Prog7311POE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prog7311POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2234089f55b84502b9368f9cf69f744a4dcabe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9271ebd69b952b195111396d5ce21bca2ece7a94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prog7311POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prog7311POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
